d875e319875fa4d371902f63135fddabc82673a6
xudan <xudan@apowo.com>
2023-07-20 12:56:34 +0800

    fix update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faCircleCheck, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faTwitter, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 
@@ -9,6 +9,6 @@ import { faTwitter, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-ic
 import './style.css'
 import './index.css'; 
 
-library.add(faUserSecret, faCircleCheck, faTwitter, faFacebook, faGoogle)
+library.add(faUserSecret, faCircleCheck, faTwitter, faFacebook, faGoogle, faBars)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
