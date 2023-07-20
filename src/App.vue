ebda775bd51efb9cb7a0ab1b7052148363570db2
xudan <xudan@apowo.com>
2023-07-20 13:20:40 +0800

    fix update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="">
-    <header class="bg-white">
+    <header class="bg-white dark:bg-[#212233e6]">
         <nav class="flex justify-between items-center w-[92%]  mx-auto py-3">
                 <div class="flex"> 
                     <div class="text-slate-950 font-bold">
@@ -8,36 +8,36 @@
                       <font-awesome-icon class="text-lime-500 dark:text-blue-400"  icon="user-secret" />
                     </div>
                     <div
-                        class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+                        class="nav-links duration-500 md:static absolute  md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
                         <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                             <li>
-                                <a class="text-black hover:text-gray-500" href="#">Discover</a>
+                                <a class="text-black dark:text-white dark:hover:text-gray-500  hover:text-gray-500" href="#">Discover</a>
                             </li>
                             <li>
-                                <a class="text-black hover:text-gray-500" href="#">Marketplace</a>
+                                <a class="text-black dark:text-white dark:hover:text-gray-500 hover:text-gray-500" href="#">Marketplace</a>
                             </li>
                             <li>
-                                <a class="text-black hover:text-gray-500" href="#">Creator</a>
+                                <a class="text-black dark:text-white dark:hover:text-gray-500 hover:text-gray-500" href="#">Creator</a>
                             </li>
                             <li>
-                                <a class="text-black hover:text-gray-500" href="#">Developers</a>
+                                <a class="text-black dark:text-white dark:hover:text-gray-500 hover:text-gray-500" href="#">Developers</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div class="flex items-center gap-3 md:gap-6">
-                    <button  class="text-white bg-blue-400 py-2 px-2 rounded">Sign in</button>
+                <div class="flex items-center gap-3 md:gap-5">
+                    <button  class="text-white bg-blue-400 py-2 px-2 rounded-full min-w-[100px]">Sign in</button>
                     <div class="w-9  md:w-12  bg-gray-500 rounded-full overflow-hidden">
                       <img class="w-full h-full object-cover"  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar" />
                     </div>
    
                     <!-- sun icon -->
-                    <svg v-if="isDark" @click="toggleIcon"  class="fill-current w-8 text-sky-900 dark:text-sky-900 cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+                    <svg v-if="isDark" @click="toggleIcon"  class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
                     
                     <!-- moon icon -->
-                    <svg v-else @click="toggleIcon" class="fill-current w-8 text-sky-900   dark:text-sky-900 cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+                    <svg v-else @click="toggleIcon" class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
                     
-                    <font-awesome-icon :icon="['fas', 'bars']" class="text-3xl cursor-pointer md:hidden text-sky-900 dark:text-sky-900"></font-awesome-icon>
+                    <font-awesome-icon :icon="['fas', 'bars']" class="text-3xl cursor-pointer md:hidden text-sky-900 dark:text-white"></font-awesome-icon>
                 </div>
             </nav>
     </header>
